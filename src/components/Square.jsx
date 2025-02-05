df757ebf444041537ad5d4a14a74d563f565febb
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-02-05 17:57:36 +0530

    tic-tac-toe app is complete

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+function Square({value,onSquareClick}){
+ 
+    return(
+        <button className="square" onClick={onSquareClick}>
+            {
+                value
+            }
+        </button>
+    )
+}
+
+export default Square;
